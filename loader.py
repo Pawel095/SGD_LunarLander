44555a360776d998a2b5dcc0bdf61c91f071db52
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2020-05-20 23:18:18 +0200

    added assets loader

---
+++ loader.py
@@ -0,0 +1,42 @@
+import pygame
+import threading
+
+assets = {}
+loadingFinished = False
+
+
+class worker(threading.Thread):
+    def __init__(self, loadFunc, path, key, args=None):
+        super().__init__()
+        self.loadFunc = loadFunc
+        self.path = path
+        self.key = key
+        self.args = args
+
+    def run(self):
+        if self.args is not None:
+            assets[self.key] = self.loadFunc(self.path, *self.args)
+        else:
+            assets[self.key] = self.loadFunc(self.path)
+
+
+class Loader:
+    def load(self):
+        threads = []
+        threads.extend([
+            worker(pygame.image.load,"assets/lander.png","lander")
+        ])
+        [t.start() for t in threads]
+        [t.join() for t in threads]
+        loadingFinished = True
+
+        if loadingFinished:
+            pass
+
+    def get(self, key):
+        try:
+            ret = assets[key]
+        except KeyError as e:
+            print(e)
+        else:
+            return ret
